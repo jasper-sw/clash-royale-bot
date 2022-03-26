53e8230f55e41b6c06b91a2e3045e9862196b949
Jasper Swensen <jasperswensen@gmail.com>
2022-03-26 02:59:31 -0600

    handle case where players don't have a clan

--- clash_bot.py
+++ clash_bot.py
@@ -5,24 +5,52 @@ import random
 class ClashBot:
     # messages to send players based on whether they lose or win, and how many crowns they get
     one_crown_loss_messages = ["[ClashBot] 1 crown... L bro",
-                               "[ClashBot] 1 crown bro... gotta do better"]
+                               "[ClashBot] 1 crown bro... gotta do better",
+                               "[ClashBot] Hey at least you got 1 tower!",
+                               "[ClashBot] Could have been worse, you could have got 0 crowns",
+                               "[ClashBot] Matchmaking is rigged bro!"
+                               "[ClashBot] L+RATIO RIP BOZO",
+                               "[ClashBot] LMAO"]
 
     two_crown_loss_messages = ["[ClashBot] 2 crown... L bro",
-                               "[ClashBot] 2 crowns... close but not quite"]
+                               "[ClashBot] 2 crowns... close but not quite",
+                               "[ClashBot] You almost had that one, only needed 1 more",
+                               "[ClashBot] Really thought you had that one",
+                               "[ClashBot] Robbed tbh",
+                               "[ClashBot] Matchmaking is rigged bro!",
+                               "[ClashBot] LMAO"]
 
     zero_crown_loss_messages = ["[ClashBot] 0 crown... L bro",
                                 "[ClashBot] No crowns no skill bro",
-                                "[ClashBot] R.I.P. 0 crowns bro"]
+                                "[ClashBot] R.I.P. 0 crowns bro",
+                                "[ClashBot] Maybe you'll do better next time",
+                                "[ClashBot] Not everyone is cut out for this, its alright",
+                                "[ClashBot] Maybe you should think about a new deck",
+                                "[ClashBot] Ass?",
+                                "[ClashBot] Matchmaking is rigged bro!",
+                                "[ClashBot] L+RATIO RIP BOZO",
+                                "[ClashBot] LMAO"]
 
     one_crown_win_messages = ["[ClashBot] 1 crown W bro!",
-                              "[ClashBot] That last one was a win... but barely"]
+                              "[ClashBot] That last one was a win... but barely",
+                              "[ClashBot] Hard fought win!",
+                              "[ClashBot] 1 crown to rule them all"]
 
     two_crown_win_messages = ["[ClashBot] 2 crown W bro!",
-                              "[ClashBot] Solid win bro"]
+                              "[ClashBot] Solid win bro",
+                              "[ClashBot] Can't hate on a 2 crown",
+                              "[ClashBot] Respectable. Have a nice day",
+                              "[ClashBot] 2 crown king right here",
+                              "[ClashBot] No BOZOs here"]
 
     three_crown_win_messages = ["[ClashBot] 3 crown W bro!",
                                 "[ClashBot] Lets gooo that's a W",
-                                "[ClashBot] Brutally destructive 3 crown"]
+                                "[ClashBot] Brutally destructive 3 crown",
+                                "[ClashBot] That guy was ass",
+                                "[ClashBot] Another day another dub",
+                                "[ClashBot] GOAT?",
+                                "[ClashBot] He didn't even stand a chance",
+                                "[ClashBot] Hog Riiiiidaaaaa"]
 
     def __init__(self):
         pass
--- entity_classes/player.py
+++ entity_classes/player.py
@@ -5,18 +5,24 @@ from entity_classes.deck import Deck
 class Player:
     username: str
     player_tag: str
-    clan_tag: str
-    clan_name: str
-    clan_badge_id: int
+    clan_tag: str or None
+    clan_name: str or None
+    clan_badge_id: int or None
     player_deck: Deck
     original_player_dict: dict
 
     def __init__(self, player_dict: dict):
         self.username = player_dict["name"]
         self.player_tag = player_dict["tag"]
-        self.clan_tag = player_dict["clan"]["tag"]
-        self.clan_name = player_dict["clan"]["name"]
-        self.clan_badge_id = player_dict["clan"]["badgeId"]
+        try:
+            self.clan_tag = player_dict["clan"]["tag"]
+            self.clan_name = player_dict["clan"]["name"]
+            self.clan_badge_id = player_dict["clan"]["badgeId"]
+        # if we get a key error for clan info, that player doesn't belong to a clan
+        except KeyError:
+            self.clan_tag = None
+            self.clan_name = None
+            self.clan_badge_id = None
         self.player_deck = Deck(list_of_card_dicts=player_dict["cards"])
         self.original_player_dict = player_dict
 
